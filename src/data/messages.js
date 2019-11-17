34c4fa726cbbb655e58a4f418febd36616e43ae3
Daryl Duckmanton <daryl@lyraddigital.com>
2019-11-17 22:02:30 +1100

    Adding remaining coversation ans messages

--- src/data/messages.js
+++ src/data/messages.js
@@ -16,5 +16,63 @@ export const messages = [
         `,
         createdAt: 'Apr 16',
         isMyMessage: false
+    },
+    {
+        imageUrl: null,
+        imageAlt: null,
+        messageText: 'Maybe we can use Jim\'s studio.',
+        createdAt: 'Apr 15',
+        isMyMessage: true
+    },
+    {
+        imageUrl: require('../images/profiles/daryl.png'),
+        imageAlt: 'Daryl Duckmanton',
+        messageText: `
+            All I know is where I live it's too hard
+            to record because of all the street noise.
+        `,
+        createdAt: 'Apr 15',
+        isMyMessage: false
+    },
+    {
+        imageUrl: null,
+        imageAlt: null,
+        messageText: `
+            Well we need to work out sometime soon where
+            we really want to record our video course.
+        `,
+        createdAt: 'Apr 15',
+        isMyMessage: true
+    },
+    {
+        imageUrl: require('../images/profiles/daryl.png'),
+        imageAlt: 'Daryl Duckmanton',
+        messageText: `
+            I'm just in the process of finishing off the
+            last pieces of material for the course.
+        `,
+        createdAt: 'Apr 15',
+        isMyMessage: false
+    },
+    {
+        imageUrl: null,
+        imageAlt: null,
+        messageText: 'How\'s it going?',
+        createdAt: 'Apr 13',
+        isMyMessage: true
+    },
+    {
+        imageUrl: require('../images/profiles/daryl.png'),
+        imageAlt: 'Daryl Duckmanton',
+        messageText: ' Hey mate what\'s up?',
+        createdAt: 'Apr 13',
+        isMyMessage: false
+    },
+    {
+        imageUrl: null,
+        imageAlt: null,
+        messageText: 'Hey Daryl?',
+        createdAt: 'Apr 13',
+        isMyMessage: true
     }
 ];
