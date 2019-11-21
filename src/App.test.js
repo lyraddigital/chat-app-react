4ad1e1fb358b810afcaf835872c76ca35c3952bb
Daryl Duckmanton <Daryl.Duckmanton@tabcorp.com.au>
2019-11-21 13:12:47 +1100

    Started looking at setting up Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('App loads with default state', () => {
+  const component = renderer.create(
+    <App />
+  );
+
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
 });
